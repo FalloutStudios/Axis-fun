7c2af235ff65daef7a89d4044d419e1aa5274002
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-29 13:11:11 +0800

    update ping language

--- src/ping.js
+++ src/ping.js
@@ -1,5 +1,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const safeMessage = require('../scripts/safeMessage');
+const Yml = require('yaml');
+const Fs = require('fs');
+
+const defaultLang = `# Axis fun (ping) language
+ping:
+  - 'Pong! <a:sadsponge:901325244353093674>'
+  - 'Pong! <a:bingusangry:901325252318089276>'
+  - 'Ping <:huh:901325246165045258>'
+  - 'Ping Ping <a:funnycursed:901325245854662667>'`;
 
 module.exports = new create();
 
@@ -8,7 +17,8 @@ function create(){
     this.versions = ['1.1.0'];
 
     this.start = (client, action, conf, lang) => {
-        language = lang;
+        if(!Fs.existsSync('./config/ping.yml')) Fs.writeFileSync('./config/ping.yml', defaultLang);
+        language = Yml.parse(Fs.readFileSync('./config/ping.yml', 'utf-8'));
 
         return true;
     }
